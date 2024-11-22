8974f1d2cd99d76bf08aede0d4875c1e1ab82b6e
Samael <33399712+bleonheart@users.noreply.github.com>
2024-11-22 12:59:44 +0000

    Made persistence slightly easier to read

--- helix_persist_toolgun/lua/weapons/gmod_tool/stools/ixpersist.lua
+++ helix_persist_toolgun/lua/weapons/gmod_tool/stools/ixpersist.lua
@@ -2,43 +2,46 @@
     Created by redcatjack
     https://github.com/redcatjack
 --]]
-
 if CLIENT then
-	TOOL.Information = {
-        { name = "info", stage = 1 },
-		{ name = "left" },
-		{ name = "right" },
-	}
-    language.Add( "tool.ixpersist.left", "Make an entity persistent" )
-	language.Add( "tool.ixpersist.right", "Remove persistence from entity" )
-    language.Add( "tool.ixpersist.name", "Helix Persistance Tool" )
-    language.Add( "tool.ixpersist.desc", "Make entities persist through restarts" )
+    TOOL.Information = {
+        {
+            name = "info",
+            stage = 1
+        },
+        {
+            name = "left"
+        },
+        {
+            name = "right"
+        },
+    }
+
+    language.Add("tool.ixpersist.left", "Make an entity persistent")
+    language.Add("tool.ixpersist.right", "Remove persistence from entity")
+    language.Add("tool.ixpersist.name", "Helix Persistance Tool")
+    language.Add("tool.ixpersist.desc", "Make entities persist through restarts")
 end
 
 TOOL.Category = "Helix"
 TOOL.Name = "#tool.ixpersist.name"
 TOOL.Description = "#tool.ixpersist.desc"
-
 if SERVER then
     util.AddNetworkString("PersistenceTool_Persist")
     util.AddNetworkString("PersistenceTool_Unpersist")
-
     net.Receive("PersistenceTool_Persist", function(len, ply)
         local entity = net.ReadEntity()
         if not IsValid(entity) then return end
-
         entity:SetPersistent(true) -- Custom function to make entity persistent
         print(ply:Nick() .. " has made " .. entity:GetClass() .. " persistent.")
-        ply:Notify("Enabled persist for ".. entity:GetClass())
+        ply:Notify("Enabled persist for " .. entity:GetClass())
     end)
 
     net.Receive("PersistenceTool_Unpersist", function(len, ply)
         local entity = net.ReadEntity()
         if not IsValid(entity) then return end
-
         entity:SetPersistent(false) -- Custom function to remove persistence
         print(ply:Nick() .. " has removed persistence from " .. entity:GetClass() .. ".")
-        ply:Notify("Removed persist for ".. entity:GetClass())
+        ply:Notify("Removed persist for " .. entity:GetClass())
     end)
 end
 
@@ -46,11 +49,9 @@ function TOOL:LeftClick(trace)
     if CLIENT then
         local entity = trace.Entity
         if not IsValid(entity) then return false end
-
         net.Start("PersistenceTool_Persist")
         net.WriteEntity(entity)
         net.SendToServer()
-
         return true
     end
 end
@@ -59,15 +60,15 @@ function TOOL:RightClick(trace)
     if CLIENT then
         local entity = trace.Entity
         if not IsValid(entity) then return false end
-
         net.Start("PersistenceTool_Unpersist")
         net.WriteEntity(entity)
         net.SendToServer()
-
         return true
     end
 end
 
 function TOOL.BuildCPanel(panel)
-    panel:AddControl("Header", {Description = "#tool.ixpersist.desc"})
+    panel:AddControl("Header", {
+        Description = "#tool.ixpersist.desc"
+    })
 end
